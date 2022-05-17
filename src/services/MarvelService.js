0d6485dadae688a0e635eb8326ed752e23c92562
Kostiuk93 <kostiuk93@gmail.com>
2022-05-17 04:03:25 +0300

    React Middle Трансформация данных и компонент со случайным персонажем

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,12 +12,24 @@ class MarvelService {
         return await res.json();
     };
 
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=300&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=300&${this._apiKey}`);
+        return res.data.results.map(this._transformCaracter);
     }
 
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`); // получаем персонажа
+        return this._transformCaracter(res.data.results[0]) // возвращаем результат персонажа
+    }
+
+    _transformCaracter = (char) => {
+        return {
+            name: char.name,
+            description: char.description  ? `${char.description.slice(0, 150)}...` : "Description is missing! Sorry!",
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url 
+        }
     }
 }
 
